712a0703d4c164001fe9129f397a640854dd2171
bskinnersf <bob.skinner@piercewashington.com>
2016-10-27 12:22:19 -0700

    initial checkin

---
+++ js/cycle.js
@@ -0,0 +1,15 @@
+	$(document).ready(function() {
+	    $('#clientStories').cycle({
+			fx: 'fade',
+			speed: 500,  // for fade effect
+			timeout: 2000    // for your image
+		});
+	});
+	
+	$(document).ready(function() {
+	    $('#clientStories2').cycle({
+			fx: 'fade',
+			speed: 500,  // for fade effect
+			timeout: 2000    // for your image
+		});
+	});
